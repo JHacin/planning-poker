f5aa78b77c0c1d8c81f18a64be4fb63667982fd5
JHacin <jan.hacin@gmail.com>
2019-09-16 23:17:26 +0200

    Start working on displaying a session page.

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -1,22 +1,81 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { SESSION_STATUS_WAITING } from "../constants";
 
 class Session extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isInitialized: false,
+      status: null,
+      id: null,
       name: "",
       scaleType: "",
-      userStories: []
+      userStories: [],
+      owner: ""
     };
   }
 
+  static getDerivedStateFromProps(props, state) {
+    if (!state.isInitialized && props.session) {
+      return {
+        ...props.session,
+        isInitialized: true,
+        status: SESSION_STATUS_WAITING
+      };
+    }
+
+    return { ...state };
+  }
+
+  initializeSessionState = props => {
+    this.setState({ ...props });
+  };
+
   render() {
-    return (
-      <div>
-        <span>Hello</span>
-      </div>
-    );
+    const { isInitialized, status, name } = this.state;
+
+    if (!isInitialized) {
+      return (
+        <div>
+          <span>Loading...</span>
+        </div>
+      );
+    }
+
+    switch (status) {
+      case SESSION_STATUS_WAITING:
+        return (
+          <div>
+            <h1>{name}</h1>
+            <h3>Waiting for participants to join...</h3>
+          </div>
+        );
+      default:
+        return <div>Could not fetch status for this session.</div>;
+    }
   }
 }
 
-export default Session;
+const mapStateToProps = (state, ownProps) => ({
+  session: state.sessions.byId[ownProps.match.params.id]
+});
+
+Session.propTypes = {
+  session: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    scaleType: PropTypes.string.isRequired,
+    userStories: PropTypes.array.isRequired,
+    owner: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(Session)
+);
