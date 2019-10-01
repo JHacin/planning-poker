74863a429a27c54306175ebda4317205ca703f23
JHacin <jan.hacin@gmail.com>
2019-10-02 00:03:08 +0200

    Fix issue with page refreshes redirecting elsewhere.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import Header from "./components/Header";
 import JoinSessions from "./components/JoinSessionsPage";
@@ -11,16 +11,20 @@ import Session from "./components/Session/Session";
 import { isLoggedIn } from "./util/user";
 
 const App = () => {
+  const catchAllRedirectLocation = isLoggedIn() ? "/join-sessions" : "/login";
+
   return (
     <Router>
       <Header />
-      <PrivateRoute path="/join-sessions" component={JoinSessions} />
-      <PrivateRoute path="/my-sessions" exact component={MySessionsPage} />
-      <PrivateRoute path="/my-sessions/add" exact component={AddSessionForm} />
-      <PrivateRoute path="/sessions/:id" exact component={Session} />
-      <PrivateRoute path="/logout" exact component={LogoutRoute} />
-      <Route path="/login" exact component={LoginPage} />
-      <Route render={() => <Redirect to={isLoggedIn() ? "/join-sessions" : "/login"} />} />
+      <Switch>
+        <Route path="/login" exact component={LoginPage} />
+        <PrivateRoute path="/join-sessions" exact component={JoinSessions} />
+        <PrivateRoute path="/my-sessions" exact component={MySessionsPage} />
+        <PrivateRoute path="/my-sessions/add" exact component={AddSessionForm} />
+        <PrivateRoute path="/sessions/:id" exact component={Session} />
+        <PrivateRoute path="/logout" exact component={LogoutRoute} />
+        <Route path="/" render={() => <Redirect to={catchAllRedirectLocation} />} />
+      </Switch>
     </Router>
   );
 };
